21f937f8defc66fdad2de3d53146935535fa1de6
songshuang <shuang.song@nearfor.me>
2016-04-27 18:45:16 +0800

    update ch1-08 solution

---
+++ Chapter1/ch1-08.scm
@@ -0,0 +1,29 @@
+(define (cube x) (* x x x))
+
+(define (close-result x y)
+  (/ (+(/ x (square y)) (* 2 x)) 3)
+ )
+
+(define (square x) (* x x))
+
+(define (cubic-root-iter guess x)
+  (if (good-enough? guess x)
+	guess
+	(cubic-root-iter (improve guess x) x)
+	)
+ )
+
+(define (improve guess x)
+ (close-result guess x)
+ )
+
+(define (good-enough? guess x)
+  (> (abs (- (cube guess) x)) 0.0001) 
+ )
+
+(define (abs x)
+ (if (< x 0) (- x) x)  
+)
+
+(define (cubic-root x) (cubic-root-iter 2.0 x))
+(cubic-root 27)
