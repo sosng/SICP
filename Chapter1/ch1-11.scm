09d4e1018c8903e5b2959f3e8991f808455f335c
Sayaka <sayaka@MacBook.local>
2016-05-02 23:29:16 +0800

    update ch1-09 10 11

---
+++ Chapter1/ch1-11.scm
@@ -0,0 +1,17 @@
+(define (f n)
+  (cond ((< n 3) n)
+		(else (+ (f (- n 1)) (* 2 (f (- n 1))) (* 3 (f (- n 3))))))
+)
+
+(define (f2 n)
+  (define (f-iter a b c n i)
+	(if (= i 0) )
+	
+	
+	) 
+  
+  
+  )
+
+
+(f 3)
