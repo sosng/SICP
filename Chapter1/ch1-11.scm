fd09eaac1cdf4c88db3d3a27d3f4d6950610a49e
Sayaka <sayaka@MacBook.local>
2016-05-05 23:03:03 +0800

    update ch1-12

--- Chapter1/ch1-11.scm
+++ Chapter1/ch1-11.scm
@@ -1,17 +1,17 @@
 (define (f n)
-  (cond ((< n 3) n)
-		(else (+ (f (- n 1)) (* 2 (f (- n 1))) (* 3 (f (- n 3))))))
-)
-
-(define (f2 n)
-  (define (f-iter a b c n i)
-	(if (= i 0) )
-	
-	
-	) 
-  
-  
+	(if (< n 3)
+	  n
+	  (+ (f (- n 1))
+		 (* 2 (f (- n 2)))
+		 (* 3 (f (- n 3)))))
   )
 
+(define (f2 n)
+  (f-iter 2 1 0 0 n))
+(define (f-iter a b c i n)
+	(if (= i n) c
+	 (f-iter (+ a (* 2 b) (* 3 c)) a b (+ i 1) n))
+)
 
-(f 3)
+(f 4)
+(f2 4)
